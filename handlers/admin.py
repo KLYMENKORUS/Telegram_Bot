5061c66dd47c755f63d0b65ad2e9a50370aec06b
KLYMENKORUS <klimenkoruslan11@gmail.com>
2022-11-03 15:44:34 +0200

    Add files

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,6 +1,5 @@
 from aiogram.dispatcher import FSMContext
-from create_bot import dp
-from aiogram import types
+from aiogram import types, Dispatcher
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
@@ -12,14 +11,14 @@ class FSMAdmin(StatesGroup):
 
 
 # Начало диалого загрузки нового пункта меню
-@dp.message_handler(commands='Загрузить', state=None)
+#@dp.message_handler(commands='Загрузить', state=None)
 async def cm_start(message: types.Message):
     await FSMAdmin.photo.set()
     await message.reply('Загрузи фото')
 
 
 # Ловим первый ответ и пишем в словарь
-@dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
+#@dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
 async def load_photo(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['photo'] = message.photo[0].file_id
@@ -28,7 +27,7 @@ async def load_photo(message: types.Message, state: FSMContext):
 
 
 # Ловим второй ответ
-@dp.message_handler(state=FSMAdmin.name)
+#@dp.message_handler(state=FSMAdmin.name)
 async def load_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
@@ -37,5 +36,29 @@ async def load_name(message: types.Message, state: FSMContext):
 
 
 # Ловим третий ответ
-@dp.message_handler(state=FSMAdmin.description)
-async def load_description(message: types.Message, state: FSMContext)
+#@dp.message_handler(state=FSMAdmin.description)
+async def load_description(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['description'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Теперь укажи цену')
+
+
+# Ловим последний ответ и используем полученные даннные
+#@dp.message_handler(state=FSMAdmin.price)
+async def load_price(messsage: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['price'] = float(messsage.text)
+
+    async with state.proxy() as data:
+        await messsage.reply(str(data))
+    await state.finish()
+
+
+# Регистрируем хендлеры
+def register_handler_admin(dp: Dispatcher):
+    dp.register_message_handler(cm_start, commands=['Загрузить'], state=None)
+    dp.register_message_handler(load_photo, content_types=['photo'], state=FSMAdmin.photo)
+    dp.register_message_handler(load_name, state=FSMAdmin.name)
+    dp.register_message_handler(load_description, state=FSMAdmin.description)
+    dp.register_message_handler(load_price, state=FSMAdmin.price)
--- pizza_bot.py
+++ pizza_bot.py
@@ -7,6 +7,8 @@ async def on_startup(_):
     print('Бот вышел в онлайн')
 
 client.register_handler_client(dp)
+admin.register_handler_admin(dp)
 other.register_handler_other(dp)
+
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
